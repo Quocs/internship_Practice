8208c5e386e65b2400c7a43b6e7fea220dd63a78
Quocs <quochades2000@gmail.com>
2022-08-22 13:49:49 +0700

    feat: Do exercise about string

---
+++ Learn-js/Strings/string.js
@@ -0,0 +1,21 @@
+const name="Falavio";
+
+//length string
+const variablea= name.length;
+
+//Emty length
+const variable_b = ''.length;
+
+//Two string
+const variable_c = 'Thanh Quoc';
+const fullname = "My name is " + variable_c;
+
+// Noi Suy Chuoi
+
+const string=`something ${1+2+3}`;
+
+console.log(variablea);
+console.log(variable_b);
+console.log(fullname);
+console.log(string);
+
