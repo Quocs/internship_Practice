55ea010008ca1cfec33822f979542e45ca6a0e65
Quocs <quochades2000@gmail.com>
2022-09-15 08:57:48 +0700

    Do show list according to each status

--- Big-practice/src/script/app.js
+++ Big-practice/src/script/app.js
@@ -1,15 +1,16 @@
-/*
-    GET VARIABLE
+/** 
+ *  GET VARIABLE
 */
 const taskInput = document.querySelector('.new-todo');
-let todos = JSON.parse(localStorage.getItem("todo-list"));
+//  getting ul element
 const taskBox = document.querySelector('.todo-list');
+//  getting li element
 let list = taskBox.childNodes;
 let editId;
-const filter = document.querySelectorAll('.filters a');
-const isAllTask=document.querySelector('#All');
-const isPedding=document.querySelector('#pedding');
-const isCompleted=document.querySelector('#completed');
+const filters = document.querySelectorAll('.filters span');
+//  getting localstorage todo-list
+let todos = JSON.parse(localStorage.getItem("todo-list"));
+
 
 /**
  * Update status after click check box
@@ -64,26 +65,27 @@ const getEdit = (taskId, taskName) => {
 /**
  * Filter task of the list
  */
-
-filter.forEach(btn => {
+filters.forEach(btn => {
     btn.addEventListener('click', () => {
-        //console.log(btn)
-        document.querySelector("a.selected").classList.remove("selected");
+        document.querySelector("span.selected").classList.remove("selected");
         btn.classList.add("selected");
-        }
-    )
+        showTask(btn.id);
+    })
 })
 
 /**
  * Show Task saved in localStorage
  */
 
-const showTask = () => {
+const showTask = (Filter) => {
     let li = "";
     if (todos) {
         todos.forEach((todo, id) => {
+            //if todostatus is completed set input is checked
             let isCompleted = todo.status == "completed" ? "checked" : "";
+            //if todostatus is completed set class li element id completed
             let isTaskCompleted = todo.status == "completed" ? "completed" : "";
+            if (Filter == todo.status || Filter == "all") {
                 li +=
                     `<li class="${isTaskCompleted}">
                 <div class="view">
@@ -93,10 +95,13 @@ const showTask = () => {
                 </div>
                 <input class="edit">         
             </li>`;
+            }
         });
     }
     taskBox.innerHTML = li;
 }
+showTask("all");
+
 /**
  *  SAVE TASK IN LOCALSTORAGE
  */
